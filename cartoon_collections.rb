afe57397eb6def5d60f451e1a4368e87f87550cf
Clark Johnson <clarkandkathy@gmail.com>
2019-09-10 20:19:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,5 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  ingredients.include?(cheese_types)
+  ingredients.include?{}
 end
